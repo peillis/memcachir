212040f330ec90274b9c279ff82502d7954ac877
peillis <enrique@markhaus.com>
2017-05-07 19:13:47 +0200

    remake benchs

--- bench/mcd_bench.exs
+++ bench/mcd_bench.exs
@@ -1,33 +1,29 @@
 Code.require_file "./bench_utils.exs", __DIR__
+
 defmodule McdBench do
   use Benchfella
   import BenchUtils
 
-  before_each_bench _ do
-    { :ok, pid} = :mcd.start_link(['localhost', 11211])
-    :timer.sleep(100)
-    { :ok, "world" } = :mcd.set(pid, "hello", "world")
-    { :ok, pid }
+  setup_all do
+    Memcachir.start([], [])
+    {:ok, "world"} = Memcachir.set("hello", "world")
+    {:ok, _} = Memcachir.set("hello_large", random_string())
+    {:ok, "hey"}
   end
 
   bench "GET" do
-    pid = bench_context
-    { :ok, "world" } = :mcd.get(pid, "hello")
+    {:ok, "world"} = Memcachir.get("hello")
   end
 
   bench "SET" do
-    pid = bench_context
-    { :ok, "world" } = :mcd.set(pid, "hello", "world")
+    {:ok, "world"} = Memcachir.set("hello", "world")
   end
 
   bench "SET LARGE", [large_blob: random_string()] do
-    pid = bench_context
-    { :ok, _ } = :mcd.set(pid, "hello", large_blob)
-    { :ok }
+    {:ok, _} = Memcachir.set("hello", large_blob)
   end
 
   bench "GET LARGE" do
-    pid = bench_context
-    { :ok, _ } = :mcd.get(pid, "hello")
+    {:ok, _} = Memcachir.get("hello_large")
   end
 end
