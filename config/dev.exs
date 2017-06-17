947770fbb6228a9e33af77689dae4f8efc2474eb
peillis <enrique@doofinder.com>
2017-06-17 18:23:47 +0200

    revert config

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :memcachir,
-  hosts: ["localhost:11211", "localhost:11212"],
-  # hosts: "localhost:11211",
+  # hosts: ["localhost:11211", "localhost:11212"],
+  hosts: "localhost:11211",
   namespace: "df:1"
