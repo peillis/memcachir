7812321d635af8136dc854c3a19077cb80148ffb
stephanos <github@stephanbehnke.de>
2018-06-12 14:51:47 -0400

    minor improvements

--- lib/memcachir/health.ex
+++ lib/memcachir/health.ex
@@ -10,7 +10,7 @@ defmodule Memcachir.HealthCheck do
   end
 
   def init(options) do
-    # only poll if using ElastiCache
+    # only need to poll if using ElastiCache
     if Keyword.has_key?(options, :elasticache) do
       schedule_health_check(options)
     end
@@ -22,11 +22,11 @@ defmodule Memcachir.HealthCheck do
     known_servers = Cluster.servers() |> Enum.into(MapSet.new)
     actual_servers = Util.get_servers(options) |> Enum.into(MapSet.new)
 
-    if not MapSet.equal?(known_servers, actual_servers) do
-      {:stop, "ElastiCache servers changed from #{inspect known_servers} to #{inspect actual_servers}", options}
-    else
+    if MapSet.equal?(known_servers, actual_servers) do
       schedule_health_check(options)
       {:noreply, options}
+    else
+      {:stop, "ElastiCache servers changed from #{inspect known_servers} to #{inspect actual_servers}", options}
     end
   end
 
