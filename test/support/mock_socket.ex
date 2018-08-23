5aa2a30a8a497e05cd450b77468f2d1a2d0f3507
Michael Guarino <mguarino@frame.io>
2018-08-23 14:33:16 -0400

    Make tests more robust

--- test/support/mock_socket.ex
+++ test/support/mock_socket.ex
@@ -33,6 +33,6 @@ defmodule MockSocketModule do
     Agent.update(__MODULE__, fn _ -> servers end)
     send(Memcachir.Cluster, :health_check)
     # wait for it to be picked up
-    Process.sleep(100)
+    Process.sleep(200)
   end
 end
